0c765b79641c823ae104faeef5e4327d41022414
Cesar Valderrama <valderramago@gmail.com>
2017-12-21 01:46:14 +0000

    Added routes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Dog Keeper Runner' },
+  { id: 12, name: 'Storm Cutter' },
+  { id: 13, name: 'Ninja Fox' },
+  { id: 14, name: 'Piggi Sticky' },
+  { id: 15, name: 'Bonds Printer' },
+  { id: 16, name: 'Snake Spy' },
+  { id: 17, name: 'Chillout Smoker' },
+  { id: 18, name: 'Booker Later' },
+  { id: 19, name: 'Glutter Spiral' },
+  { id: 20, name: 'Stars Flyer' }
+];
